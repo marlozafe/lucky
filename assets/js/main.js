b3a73868c5b8470c2542e974e6777a97073442cb
Zafe <mar19arcz@gmail.com>
2021-11-18 00:26:47 +0800

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,22 @@
+const products = document.getElementsByClassName("product");
+const modal = document.getElementById("product-modal");
+
+
+modal.querySelector("span.close").onclick = function(){
+    modal.style.display = "none";
+}
+
+/* Product onclick handler */
+for (let prod of products) {
+    prod.onclick = function(){
+        const content = prod.innerHTML;
+        modal.querySelector("div.content").innerHTML = content;
+        modal.style.display = "flex";
+    }
+}
+
+/* Back to top */
+document.getElementById("back-to-top-btn").onclick = function(){
+    document.documentElement.scrollTop = 0;
+}
+
